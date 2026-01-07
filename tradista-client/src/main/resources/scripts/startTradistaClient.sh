5b823a971c28b10d1a2e81c52590f0c3ee2fea80
Olivier <olivier.asuncion@tradista.finance>
2026-01-07 14:26:25 +0100

    #278 Improve the development environment setup procedure

--- tradista-client/src/main/resources/scripts/startTradistaClient.sh
+++ tradista-client/src/main/resources/scripts/startTradistaClient.sh
@@ -10,3 +10,5 @@ source "$SCRIPT_HOME/../../../../../tradista-scripts-common/env/tradista-env.sh"
 CLIENTLIBS_DIR="$SCRIPT_HOME/../../../../target/app-client-$TRADISTA_VERSION-bin/app-client-$TRADISTA_VERSION/lib"
 
 java -Djava.naming.factory.initial=org.wildfly.naming.client.WildFlyInitialContextFactory -Djava.naming.provider.url=remote+http://localhost:8080 -Dprism.allowhidpi=false --add-modules=javafx.controls,javafx.media,javafx.fxml --module-path "$CLIENTLIBS_DIR/javafx:$CLIENTLIBS_DIR/javafx/linux" -cp "$CLIENTLIBS_DIR/*" org.eclipse.tradista.core.common.ui.view.MainEntry
+
+echo "Tradista Client started."
